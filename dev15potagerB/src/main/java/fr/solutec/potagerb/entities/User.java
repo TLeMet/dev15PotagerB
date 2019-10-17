8c1772348de0dc6a233b1e3e7bca478cfbfa8227
TLeMet <thomas@lemetayer.fr>
2019-10-17 15:12:14 +0200

    created User class, repository and rest

--- dev15potagerB/src/main/java/fr/solutec/potagerb/entities/User.java
+++ dev15potagerB/src/main/java/fr/solutec/potagerb/entities/User.java
@@ -1,5 +1,110 @@
 package fr.solutec.potagerb.entities;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
 
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String pseudo;
+	private String nom;
+	private String prenom;
+	private String mail;
+	private String pw;
+	private String tel;
+	private int age;
+	
+	@ManyToOne
+	private Terrain terrain;
+
+	public User(String pseudo, String nom, String prenom, String mail, String pw, String tel, int age) {
+		super();
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.mail = mail;
+		this.pw = pw;
+		this.tel = tel;
+		this.age = age;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Terrain getTerrain() {
+		return terrain;
+	}
+
+	public void setTerrain(Terrain terrain) {
+		this.terrain = terrain;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", pseudo=" + pseudo + ", nom=" + nom + ", prenom=" + prenom + ", mail=" + mail
+				+ ", pw=" + pw + ", tel=" + tel + ", age=" + age + ", terrain=" + terrain + "]";
+	}
+	
 }
--- dev15potagerB/src/main/java/fr/solutec/potagerb/rest/UserRest.java
+++ dev15potagerB/src/main/java/fr/solutec/potagerb/rest/UserRest.java
@@ -1,5 +1,46 @@
 package fr.solutec.potagerb.rest;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import fr.solutec.potagerb.dao.UserRepository;
+import fr.solutec.potagerb.entities.User;
+
+@RestController
+@CrossOrigin("*")
 public class UserRest {
 
+	@Autowired
+	private UserRepository userRep;
+	
+	@RequestMapping(value="/users", method=RequestMethod.GET)
+	public List<User> getAllUsers(){
+		return (List<User>) userRep.findAll();
+	}
+	
+	@RequestMapping(value="/users/{id}", method=RequestMethod.GET)
+	public Optional<User> getUser(@PathVariable Long id) {
+		return userRep.findById(id); 
+	}
+	
+	@RequestMapping(value="/users", method=RequestMethod.POST)
+	public User saveUser(@RequestBody User u) {
+		return userRep.save(u);
+	}
+	
+	@RequestMapping(value="/users/{id}", method=RequestMethod.DELETE)
+	public boolean deleteUser(@PathVariable Long id) {
+		userRep.deleteById(id);
+		return true;
+	}
+	
+	@RequestMapping(value="/users/{id}", method=RequestMethod.PUT)
+	public User modifUser(@PathVariable Long id, @RequestBody User u) {
+		u.setId(id);
+		return userRep.save(u);
+	}
+	
 }
