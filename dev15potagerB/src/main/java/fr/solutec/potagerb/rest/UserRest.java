e55f17da9597369d866f5f401798d0cf9d55df40
TLeMet <thomas@lemetayer.fr>
2019-10-17 17:08:55 +0200

    annotations UserRest

--- dev15potagerB/src/main/java/fr/solutec/potagerb/rest/UserRest.java
+++ dev15potagerB/src/main/java/fr/solutec/potagerb/rest/UserRest.java
@@ -16,32 +16,38 @@ public class UserRest {
 	@Autowired
 	private UserRepository userRep;
 	
+	// Affichage de tous les users
 	@RequestMapping(value="/users", method=RequestMethod.GET)
 	public List<User> getAllUsers(){
 		return (List<User>) userRep.findAll();
 	}
 	
+	// Recherche user par id
 	@RequestMapping(value="/users/{id}", method=RequestMethod.GET)
 	public Optional<User> getUserbyId(@PathVariable Long id) {
 		return userRep.findById(id); 
 	}
 	
+	// Recherche user par mail
 	@RequestMapping(value="/users/{mail}", method=RequestMethod.GET)
-	public List<User> getUserbymMail(@PathVariable String mail) {
+	public List<User> getUserbyMail(@PathVariable String mail) {
 		return userRep.findByMail(mail); 
 	}
 	
+	// Ajout user
 	@RequestMapping(value="/users", method=RequestMethod.POST)
 	public User saveUser(@RequestBody User u) {
 		return userRep.save(u);
 	}
 	
+	// Delete user par id
 	@RequestMapping(value="/users/{id}", method=RequestMethod.DELETE)
 	public boolean deleteUser(@PathVariable Long id) {
 		userRep.deleteById(id);
 		return true;
 	}
 	
+	// Edit user par id
 	@RequestMapping(value="/users/{id}", method=RequestMethod.PUT)
 	public User editUser(@PathVariable Long id, @RequestBody User u) {
 		u.setId(id);
