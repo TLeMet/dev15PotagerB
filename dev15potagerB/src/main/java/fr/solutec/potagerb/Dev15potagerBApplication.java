5b321cea023ad80cd9480d3ec6f5d6ae10f81ca4
TLeMet <thomas@lemetayer.fr>
2019-10-17 12:36:24 +0200

    commit initial du Back

---
+++ dev15potagerB/src/main/java/fr/solutec/potagerb/Dev15potagerBApplication.java
@@ -0,0 +1,13 @@
+package fr.solutec.potagerb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dev15potagerBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dev15potagerBApplication.class, args);
+	}
+
+}
